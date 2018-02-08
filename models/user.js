b303c839c8e24acc8a72c653a51d61bc6afe03f8
Samani Mukhtar <201600750@bhasvic.ac.uk>
2018-02-08 11:45:10 +0000

    Created the collection for habits

--- models/user.js
+++ models/user.js
@@ -3,13 +3,30 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema(
-  {
-    _id: Number,
-    name: {type: String, required: true},
-    photoUrl: {type: String, required: true}
-  },
-  {collection: 'users'}
+    {
+       _id: Number,
+       name: {type: String, required: true},
+       photoUrl: {type: String, required: true}
+    },
+    {collection: 'users'}
 );
+
+var habitSchema = new Schema(
+    {
+        owner: Number,
+        weeks: [weekSchema],
+        CurrentStreak: Number,
+        maxStreak: Number,
+    },
+    { collection: 'habits' }
+);
+
+var weekSchema = new Schema(
+    {
+        the dates
+    },
+);
+
 
 var User = module.exports = mongoose.model('User', userSchema);
 
