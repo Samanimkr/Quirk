954a291faca8583eb064e8fb545405e4326ee737
Samanimkr <Samanimkr@Outlook.com>
2018-03-24 14:05:04 +0000

    merging

--- public/js/index.js
+++ public/js/index.js
@@ -1,85 +1,89 @@
 //Facebook analytics
 window.fbAsyncInit = function() {
     FB.init({
-      appId      : '130915754333328',
-      xfbml      : true,
-      version    : 'v2.10'
+        appId: '130915754333328',
+        xfbml: true,
+        version: 'v2.10'
     });
 
     FB.AppEvents.logPageView();
 
-  };
+};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var animSpeed = 200;
+    var animSpeed = 200;
 
-  //When 'Add habit' button is pressed => drop down menu
-  $('header a#add_habit').click(function(){
-    $('.content#dashboard_content .habit_tab').slideToggle(animSpeed); //Slide toggle the drop down menu
-  });
+    //When 'Add habit' button is pressed => drop down menu
+    $('header a#add_habit').click(function() {
+        $('.content#dashboard_content .habit_tab').slideToggle(animSpeed); //Slide toggle the drop down menu
+    });
 
-  $('header a#edit_habit').click(function(){
-    $('.content#stats_content .habit_tab').slideToggle(animSpeed); //Slide toggle the drop down menu
-  });
+    $('header a#edit_habit').click(function() {
+        $('.content#stats_content .habit_tab').slideToggle(animSpeed); //Slide toggle the drop down menu
+    });
 
     getHabits();
-    function getHabits(){
+
+    function getHabits() {
         axios.get('/gethabits')
-        .then(response => {
-            var data = response.data;
-            var html;
-            for (var habit = 0; habit < data.length; habit++) {
+            .then(response => {
+                var data = response.data;
+                var html;
+                for (var habit = 0; habit < data.length; habit++) {
 
-                var d = new Date();
-                d.setDate(d.getDate() - 2);
+                    var d = new Date();
+                    d.setDate(d.getDate() - 2);
 
-                for (var i=0; i<5; i++){
-                    var dd = d.getDate();
-                    var mm = d.getMonth()+1;
-                    var yyyy = d.getFullYear();
-                    if (dd<10) dd = "0" + dd;
-                    if (mm<10) mm = "0" + mm;
-                    var currentDay = dd+"/"+mm+"/"+yyyy;
-                    if (data[habit].datesCompleted.includes(currentDay)) {
-                        $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#ddffdd");
-                    } else if (data[habit].datesFailed.includes(currentDay)){
-                        $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#ffdddd");
-                    } else {
-                        $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#FFF");
+                    for (var i = 0; i < 5; i++) {
+                        var dd = d.getDate();
+                        var mm = d.getMonth() + 1;
+                        var yyyy = d.getFullYear();
+                        if (dd < 10) dd = "0" + dd;
+                        if (mm < 10) mm = "0" + mm;
+                        var currentDay = dd + "/" + mm + "/" + yyyy;
+                        if (data[habit].datesCompleted.includes(currentDay)) {
+                            $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#ddffdd");
+                        } else if (data[habit].datesFailed.includes(currentDay)) {
+                            $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#ffdddd");
+                        } else {
+                            $(`li#HabitID_${data[habit]._id} #habit_middle ul li:nth-of-type(${i+1})`).css("backgroundColor", "#FFF");
+                        }
+                        d.setDate(d.getDate() + 1);
                     }
-                    d.setDate(d.getDate()+1);
                 }
-            }
-            getDays();
-        }).catch(err => {
-            console.log(err);
-        });
+                getDays();
+            }).catch(err => {
+                console.log(err);
+            });
     }
 
-    function getDays(){
+    function getDays() {
         var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var today = new Date();
-        var currentDay = (today.getDay() > 1) ? today.getDay()-2 : today.getDay()-2+7; //gets the position of the name in the array days
+        var currentDay = (today.getDay() > 1) ? today.getDay() - 2 : today.getDay() - 2 + 7; //gets the position of the 1st day
 
         for (var i = 1; i <= 5; i++) {
             $(`li #habit_middle ul li:nth-of-type(${i})`).text(days[currentDay]);
             currentDay++;
-            if (currentDay > 6) currentDay-=7;
+            if (currentDay > 6) currentDay -= 7;
         }
         $(`li #habit_middle ul li:nth-of-type(3)`).text("Today");
     }
 
 
-    $(document).on('click', '.content#dashboard_content .habits ul li #habit_middle ul li', function(){
+    $(document).on('click', '.content#dashboard_content .habits ul li #habit_middle ul li', function() {
         console.log("clicked");
         var posting = false;
         var dayClicked = $(this).index();
@@ -89,51 +93,51 @@ $(document).ready(function(){
 
         console.log("date: " + date);
         // Send a POST request
-        if(!posting){
-            posting=true;
+        if (!posting) {
+            posting = true;
             $(".content#dashboard_content .habits ul li #habit_middle ul li").attr("disabled", true);
             axios.post('/updatehabit', {
-                habit_id: id,
-                date: date
-            })
-            .then(function (response) {
-                posting=false;
-                $(".content#dashboard_content .habits ul li #habit_middle ul li").attr("disabled", false);
-                if(response.data == "completed"){
-                    $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#ddffdd");
-                } else if (response.data == "failed"){
-                    $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#ffdddd");
-                } else {
-                    $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#fff");
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+                    habit_id: id,
+                    date: date
+                })
+                .then(function(response) {
+                    posting = false;
+                    $(".content#dashboard_content .habits ul li #habit_middle ul li").attr("disabled", false);
+                    if (response.data == "completed") {
+                        $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#ddffdd");
+                    } else if (response.data == "failed") {
+                        $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#ffdddd");
+                    } else {
+                        $(`li#${habit_name} #habit_middle ul li:nth-of-type(${dayClicked+1})`).css("backgroundColor", "#fff");
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
         }
 
     })
 
-    function getDate(dayClicked){
+    function getDate(dayClicked) {
         var today = new Date();
-        var dateOffset = (24*60*60*1000) * (2-dayClicked); //5 days
+        var dateOffset = (24 * 60 * 60 * 1000) * (2 - dayClicked); //5 days
         today.setTime(today.getTime() - dateOffset);
 
         var dd = today.getDate();
-        var mm = today.getMonth()+1;
+        var mm = today.getMonth() + 1;
         var yyyy = today.getFullYear();
-        if(dd<10) dd='0'+dd;
-        if(mm<10) mm='0'+mm;
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
 
         return `${dd}/${mm}/${yyyy}`;
     }
 
-    $("#nav_stats_link").click(function(){
+    $("#nav_stats_link").click(function() {
         $(".StatsModal").show(animSpeed);
         $(".StatsModalBackground").fadeIn(animSpeed);
     });
 
-    $(".StatsModalBackground").click(function(){
+    $(".StatsModalBackground").click(function() {
         $(".StatsModal").hide(animSpeed);
         $(".StatsModalBackground").fadeOut(animSpeed);
     });
@@ -141,20 +145,20 @@ $(document).ready(function(){
 
 
     // ======================================{ Stats Page }===========================================]
-    if (window.location.pathname.substr(0,6) == "/stats"){
+    if (window.location.pathname.substr(0, 6) == "/stats") {
 
-        var habit_id = window.location.pathname.substr(7,24)
+        var habit_id = window.location.pathname.substr(7, 24)
         axios.get('/gethabits')
-        .then(response => loadCharts(response.data))
-        .catch(e => {
-            console.log(e);
-        });
+            .then(response => loadCharts(response.data))
+            .catch(e => {
+                console.log(e);
+            });
 
 
     }
 
 
-    function loadCharts(data){
+    function loadCharts(data) {
         var index = data.findIndex(i => i._id == habit_id)
         var habit = data[index];
         var stats = {
@@ -163,8 +167,8 @@ $(document).ready(function(){
             totalDays: habit.weekly_goal * habit.num_of_weeks
         }
         stats.daysMarked = stats.daysCompleted + stats.daysFailed;
-        stats.successRate = Math.round((stats.daysCompleted/stats.daysMarked)*100);
-        stats.daysLeft = stats.totalDays-stats.daysMarked < 0 ? 0 : stats.totalDays-stats.daysMarked;
+        stats.successRate = Math.round((stats.daysCompleted / stats.daysMarked) * 100);
+        stats.daysLeft = stats.totalDays - stats.daysMarked < 0 ? 0 : stats.totalDays - stats.daysMarked;
 
 
         document.getElementById("#success_rate").innerText = stats.successRate + "%";
--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,16 @@ var dateFormat = require('dateformat');
 
 module.exports = (app) => {
 
-	// middleware function to check for logged-in users
-	var sessionChecker = (req, res, next) => {
-	  if (!req.session.user) {
-		res.redirect('/login');
-	  } else {
-		next();
-	  }
-	};
+    // middleware function to check for logged-in users
+    var sessionChecker = (req, res, next) => {
+        if (!req.session.user) {
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    };
 
-    app.get('/login', function (req, res, next) {
+    app.get('/login', function(req, res, next) {
         if (req.session.user) {
             res.redirect("/");
         } else {
@@ -23,194 +23,205 @@ module.exports = (app) => {
         }
     });
 
-	app.get('/logout', function (req, res, next) {
-		req.session.destroy();
-		res.redirect('/login');
+    app.get('/logout', function(req, res, next) {
+        req.session.destroy();
+        res.redirect('/login');
     });
 
-    app.get('/', sessionChecker, function (req, res, next) {
+    app.get('/', sessionChecker, function(req, res, next) {
         var LoggedInUser = req.session.user;
 
-        User.findOne({ '_id': LoggedInUser }, function (err, user) {
-			Habit.find({'owner': LoggedInUser}, function(err, habits){
-				res.render('dashboard', {
-					title: "Quirk - Dashboard",
-					user: user,
-					habits: habits
-				});
-			});
+        User.findOne({
+            '_id': LoggedInUser
+        }, function(err, user) {
+            Habit.find({
+                'owner': LoggedInUser
+            }, function(err, habits) {
+                res.render('dashboard', {
+                    title: "Quirk - Dashboard",
+                    user: user,
+                    habits: habits
+                });
+            });
         });
-  	});
+    });
 
-	app.get('/stats/:id', sessionChecker, function (req, res, next) {
-		var LoggedInUser = req.session.user;
-		var habit_id = req.params.id;
+    app.get('/stats/:id', sessionChecker, function(req, res, next) {
+        var LoggedInUser = req.session.user;
+        var habit_id = req.params.id;
 
-
-        User.findOne({ '_id': LoggedInUser }, function (err, user) {
-			Habit.find({'owner': LoggedInUser}, async function(err, habits){
-				habit = await habits[habits.findIndex(i => i._id == habit_id)];
-				await sortHabit(habit);
-				var stats = await getStreaks(habit);
-				res.render('statistics', {
-					title: "Quirk - Statistics",
-					user: user,
-					habit: habit,
-					habits: habits,
-					stats: stats
-				});
-
-			});
+        User.findOne({'_id': LoggedInUser}, function(err, user) {
+            Habit.find({'owner': LoggedInUser}, async function(err, habits) {
+                habit = await habits[habits.findIndex(i => i._id == habit_id)];
+                await sortHabit(habit);
+                var stats = await getStreaks(habit);
+                res.render('statistics', {
+                    title: "Quirk - Statistics",
+                    user: user,
+                    habit: habit,
+                    habits: habits,
+                    stats: stats
+                });
+            });
         });
-  	});
+    });
 
-	app.post('/stats/:id/edit', sessionChecker, function(req, res, next){
-		var LoggedInUser = req.session.user;
-		var habit_id = req.params.id;
+    app.post('/stats/:id/edit', sessionChecker, function(req, res, next) {
+        var LoggedInUser = req.session.user;
+        var habit_id = req.params.id;
 
-		Habit.findOne({'_id': habit_id}, function(err, habit){
-			habit.habit_name = req.body.habitName;
-			habit.habit_desc = req.body.habitDesc;
-			habit.weekly_goal = req.body.weeklyGoal;
-			habit.num_of_weeks = req.body.numOfWeeks;
-			habit.colour = req.body.colour;
-			habit.save();
-			res.redirect(`/stats/${habit_id}`);
-		});
-	});
+        Habit.findOne({
+            '_id': habit_id
+        }, function(err, habit) {
+            habit.habit_name = req.body.habitName;
+            habit.habit_desc = req.body.habitDesc;
+            habit.weekly_goal = req.body.weeklyGoal;
+            habit.num_of_weeks = req.body.numOfWeeks;
+            habit.colour = req.body.colour;
+            habit.save();
+            res.redirect(`/stats/${habit_id}`);
+        });
+    });
 
-	app.get('/stats/:id/delete', sessionChecker, function (req, res, next) {
-		var LoggedInUser = req.session.user;
-		var habit_id = req.params.id;
+    app.get('/stats/:id/delete', sessionChecker, function(req, res, next) {
+        var LoggedInUser = req.session.user;
+        var habit_id = req.params.id;
 
-		Habit.findOne({'_id': habit_id}).remove().exec().then(() => {
-			res.redirect("/");
-		})
-  	});
+        Habit.findOne({
+            '_id': habit_id
+        }).remove().exec().then(() => {
+            res.redirect("/");
+        })
+    });
 
-  app.post('/addhabit', function(req, res, next){
-    var newHabit = {
-      owner: req.session.user,
-      habit_name: req.body.habitName,
-      habit_desc: req.body.habitDesc,
-      weekly_goal: req.body.weeklyGoal,
-	  num_of_weeks: req.body.numOfWeeks,
-	  colour: req.body.colour
+    app.post('/addhabit', function(req, res, next) {
+        var newHabit = {
+            owner: req.session.user,
+            habit_name: req.body.habitName,
+            habit_desc: req.body.habitDesc,
+            weekly_goal: req.body.weeklyGoal,
+            num_of_weeks: req.body.numOfWeeks,
+            colour: req.body.colour
+        }
+
+        var habit = new Habit(newHabit);
+        habit.save()
+            .then((savedHabit) => {
+                res.redirect('/');
+            }).catch((error) => {
+                console.log(error);
+                res.redirect('/');
+            });
+    });
+
+
+    app.get('/gethabits', function(req, res) {
+        Habit.find({
+            owner: req.session.user
+        }, function(err, habits) {
+            res.json(habits);
+        });
+    });
+
+    app.post('/updatehabit', function(req, res) {
+        Habit.findOne({
+            _id: req.body.habit_id
+        }, function(err, habit) {
+            if (habit.datesCompleted.includes(req.body.date)) { //if it already exists in the array => remove it =>(else) add it
+                var index = habit.datesCompleted.indexOf(req.body.date);
+                habit.datesFailed.push(req.body.date);
+                habit.datesCompleted.splice(index);
+                habit.save()
+                    .then(() => {
+                        res.send("failed");
+                    })
+            } else if (habit.datesFailed.includes(req.body.date)) {
+                var index = habit.datesFailed.indexOf(req.body.date);
+                habit.datesFailed.splice(index);
+                habit.save()
+                    .then(() => {
+                        res.send("removed");
+                    })
+            } else {
+                habit.datesCompleted.push(req.body.date);
+                habit.save()
+                    .then(() => {
+                        res.send("completed");
+                    })
+            }
+        });
+    });
+
+    function sortHabit(habit) {
+        habit.datesCompleted.sort(function(a, b) {
+            var c = stringToDate(a, "dd/mm/yyyy", "/");
+            var d = stringToDate(b, "dd/mm/yyyy", "/");
+            return d.getTime() - c.getTime();
+        });
+
+        habit.datesFailed.sort(function(a, b) {
+            var c = stringToDate(a, "dd/mm/yyyy", "/");
+            var d = stringToDate(b, "dd/mm/yyyy", "/");
+            return d.getTime() - c.getTime();
+        });
+        habit.save();
     }
 
-	var habit = new Habit(newHabit);
-	habit.save()
-	.then((savedHabit) => {
-      res.redirect('/');
-    }).catch((error) => {
-      console.log(error);
-      res.redirect('/');
-    });
-  });
+    async function getStreaks(habit) {
+        var streak = 0,
+            maxStreak = 0,
+            currentStreak = -1;
+        var failedDate, completedDate;
+        var temp = 0;
+        for (var i = 0; i <= habit.datesFailed.length; i++) {
+            if (habit.datesFailed.length == 0 || i == habit.datesFailed.length) {
+                failedDate = 0;
+            } else {
+                failedDate = stringToDate(habit.datesFailed[i], "dd/mm/yyyy", "/").getTime();
+            }
+
+            for (var j = temp; j < habit.datesCompleted.length; j++) {
+                completedDate = stringToDate(habit.datesCompleted[j], "dd/mm/yyyy", "/").getTime();
+                if (completedDate > failedDate) {
+                    streak++;
+                    temp = j + 1;
+                } else {
+                    if (currentStreak == -1) {
+                        currentStreak = streak;
+                    }
+                    maxStreak = streak > maxStreak ? streak : maxStreak;
+                    streak = 0;
+                }
+            }
+            maxStreak = streak > maxStreak ? streak : maxStreak;
+        }
+        if (currentStreak == -1) {
+            currentStreak = maxStreak;
+        }
+        stats = {
+            currentStreak: currentStreak,
+            maxStreak: maxStreak
+        }
+        return stats;
+    }
 
 
-  app.get('/gethabits', function(req,res){
-	  Habit.find({owner: req.session.user}, function(err, habits){
-		  res.json(habits);
-	  });
-  });
-
-  app.post('/updatehabit', function(req,res){
-	  Habit.findOne({_id: req.body.habit_id}, function(err, habit){
-		  if(habit.datesCompleted.includes(req.body.date)){ //if it already exists in the array => remove it =>(else) add it
-			  var index = habit.datesCompleted.indexOf(req.body.date);
-			  habit.datesFailed.push(req.body.date);
-			  habit.datesCompleted.splice(index);
-			  habit.save()
-			  .then(() => {
-				  res.send("failed");
-			  })
-		  } else if (habit.datesFailed.includes(req.body.date)) {
-			  var index = habit.datesFailed.indexOf(req.body.date);
-			  habit.datesFailed.splice(index);
-			  habit.save()
-			  .then(() => {
-				  res.send("removed");
-			  })
-		  } else {
-			  habit.datesCompleted.push(req.body.date);
-			  habit.save()
-			  .then(() => {
-				  res.send("completed");
-			  })
-		  }
-	  });
-  });
-
-  function sortHabit(habit){
-	  habit.datesCompleted.sort(function(a,b){
-		  var c = stringToDate(a, "dd/mm/yyyy", "/");
-		  var d = stringToDate(b, "dd/mm/yyyy", "/");
-		  return d.getTime() - c.getTime();
- 	  });
-
-	  habit.datesFailed.sort(function(a,b){
-		  var c = stringToDate(a, "dd/mm/yyyy", "/");
-		  var d = stringToDate(b, "dd/mm/yyyy", "/");
-		  return d.getTime() - c.getTime();
- 	  });
-	  habit.save();
-  }
-
-  async function getStreaks(habit){
-	  var streak = 0, maxStreak = 0, currentStreak=-1;
-	  var failedDate, completedDate;
-	  var temp=0;
-	  for (var i=0; i<=habit.datesFailed.length; i++) {
-		  if (habit.datesFailed.length==0 || i==habit.datesFailed.length) {
-			  failedDate = 0;
-		  } else {
-			  failedDate = stringToDate(habit.datesFailed[i],"dd/mm/yyyy","/").getTime();
-		  }
-
-		  for (var j=temp; j<habit.datesCompleted.length; j++) {
-    	  	completedDate = stringToDate(habit.datesCompleted[j],"dd/mm/yyyy","/").getTime();
-			if (completedDate>failedDate) {
-				streak++;
-				temp=j+1;
-			} else {
-				if (currentStreak==-1) {
-					currentStreak=streak;
-				}
-				maxStreak = streak>maxStreak? streak : maxStreak;
-				streak = 0;
-			}
-    	  }
-		  maxStreak = streak>maxStreak? streak : maxStreak;
-	  }
-	  if (currentStreak==-1) {
-		  currentStreak=maxStreak;
-	  }
-	  stats = {
-		  currentStreak: currentStreak,
-		  maxStreak: maxStreak
-	  }
-	  return stats;
-  }
-
-
-	function stringToDate(_date,_format,_delimiter)
-	{
-        var formatLowerCase=_format.toLowerCase();
-        var formatItems=formatLowerCase.split(_delimiter);
-        var dateItems=_date.split(_delimiter);
-        var monthIndex=formatItems.indexOf("mm");
-        var dayIndex=formatItems.indexOf("dd");
-        var yearIndex=formatItems.indexOf("yyyy");
-        var month=parseInt(dateItems[monthIndex]);
-        month-=1;
-        var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
+    function stringToDate(_date, _format, _delimiter) {
+        var formatLowerCase = _format.toLowerCase();
+        var formatItems = formatLowerCase.split(_delimiter);
+        var dateItems = _date.split(_delimiter);
+        var monthIndex = formatItems.indexOf("mm");
+        var dayIndex = formatItems.indexOf("dd");
+        var yearIndex = formatItems.indexOf("yyyy");
+        var month = parseInt(dateItems[monthIndex]);
+        month -= 1;
+        var formatedDate = new Date(dateItems[yearIndex], month, dateItems[dayIndex]);
         return formatedDate;
-		//https://stackoverflow.com/questions/5619202/converting-string-to-date-in-js
-	}
+        //https://stackoverflow.com/questions/5619202/converting-string-to-date-in-js
+    }
 
-  function isInArray(value, array) {
-	  return array.indexOf(value) > -1;
-  }
+    function isInArray(value, array) {
+        return array.indexOf(value) > -1;
+    }
 
 }
